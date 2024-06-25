b09bf13bfd3473457879f6cae8550121bc8ea0cf
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2024-06-26 07:43:28 +0800

    try: change config.nix

--- config.nix
+++ config.nix
@@ -1,21 +1,16 @@
 {
   packageOverrides = pkgs: with pkgs; {
     myPackages = pkgs.buildEnv {
-      name = "mischa-tools";
+      name = "marc-tools";
       paths = [
+        python3
         bash-completion
         neovim
-        go
-        nodejs_22
         starship
         fd
         ripgrep
         fzf
         lazygit
-        kubectl
-        kubectx
-        k9s
-        fluxcd
       ];
     };
   };
